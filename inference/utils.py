c0aff3e9ab16d13bd7f82f624f637dce3a566ffb
yyccbb <yuchenbo0130@gmail.com>
2026-03-01 21:10:48 +0800

    Add inference utils

---
+++ inference/utils.py
@@ -0,0 +1,22 @@
+from transformers import AutoTokenizer
+from rich import print
+
+def generate_model_response(model, tokenizer, messages):
+    chat_template = tokenizer.apply_chat_template(
+        messages,
+        tokenize=False,
+        add_generation_prompt=True
+    )
+    print(f"[bold green]{chat_template}[/bold green]")
+
+    # tokenize
+    inputs = tokenizer(chat_template, return_tensors="pt")
+    print(f"Input ids shape: {inputs['input_ids'].shape}, first 10 tokens: {inputs['input_ids'][:, :10]}")
+
+    # generate response
+    outputs = model.generate(**inputs, max_new_tokens=512)
+    input_length = inputs['input_ids'].shape[1]
+    generated_tokens = outputs[:, input_length:]
+
+    decoded_output = tokenizer.batch_decode(generated_tokens)[0]
+    return decoded_output
